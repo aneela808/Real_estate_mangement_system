146c0675238d741b19e6a7addc1cc5e5456c6f28
aneela808 <aneela15474@gmail.com>
2025-12-02 14:00:29 +0500

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "real_estate";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8");
+
+// Removed: echo "Connected!";  // This was causing header issues when included in other files
+?>
